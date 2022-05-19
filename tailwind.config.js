bac9a49e30527fd4fbe4bdc8b9440f819378c1ec
n-rdy <giuseppegelardi@icloud.com>
2022-05-19 15:57:08 -0400

    install deps

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./app/**/*.{ts,tsx,js,jsx}'],
+  darkMode: 'media', // class='light' or media='dark'
+  theme: {
+    screens: {
+      sm: '850px',
+      xm: '1020px',
+      md: '1300px',
+      lg: '1440px',
+      xl: '2560px',
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
