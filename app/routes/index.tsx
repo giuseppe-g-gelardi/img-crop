5ba5e457fb62fc61136b7b6a901ec16d49d4570f
n-rdy <giuseppegelardi@icloud.com>
2022-05-19 16:16:13 -0400

    set up upload to cloudinary example

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,77 @@
+import type { ActionFunction, UploadHandler } from "@remix-run/node";
+import {
+  json,
+  unstable_composeUploadHandlers as composeUploadHandlers,
+  unstable_createMemoryUploadHandler as createMemoryUploadHandler,
+  unstable_parseMultipartFormData as parseMultipartFormData,
+} from "@remix-run/node";
+import { Form, useActionData } from "@remix-run/react";
+
+import { uploadImage } from "~/utils/utils.server";
+
+type ActionData = {
+  errorMsg?: string;
+  imgSrc?: string;
+  imgDesc?: string;
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  const uploadHandler: UploadHandler = composeUploadHandlers(
+    async ({ 
+      name, 
+      // contentType, 
+      data, 
+      // filename 
+    }) => {
+      if (name !== "img") {
+        return undefined;
+      }
+      const uploadedImage: any = await uploadImage(data)
+      return uploadedImage.secure_url;
+    },
+    createMemoryUploadHandler()
+  );
+
+  const formData = await parseMultipartFormData(request, uploadHandler);
+  const imgSrc = formData.get("img");
+  const imgDesc = formData.get("desc");
+
+  async function logger(arg2: any, arg3: any): Promise<any> {
+    console.log('imgSrc ______', arg2)
+    console.log('imgSrc STRING', arg3)
+  }
+  
+  if (!imgSrc) {
+    return json({
+      error: "something wrong",
+    });
+  }
+  return json({
+    imgSrc,
+    imgDesc,
+  }, 
+    await logger(imgSrc, imgSrc.toString())
+  );
+};
+
 export default function Index() {
+  const data = useActionData<ActionData>();
   return (
-    <div>
-      <h1 className="text-rose-400">Hello, Remix!</h1>
-    </div>
+    <>
+      <Form method="post" encType="multipart/form-data">
+        <label htmlFor="img-field">Image to upload</label>
+        <input id="img-field" type="file" name="img" accept="image/*" />
+        <label htmlFor="img-desc">Image description</label>
+        <input id="img-desc" type="text" name="desc" />
+        <button type="submit">upload to cloudinary</button>
+      </Form>
+      {data?.errorMsg && <h2>{data.errorMsg}</h2>}
+      {data?.imgSrc && (
+        <>
+          <h2>uploaded image</h2>
+          <img src={data.imgSrc} alt={data.imgDesc || "Upload result"} />
+        </>
+      )}
+    </>
   );
 }
---
+++ app/utils/utils.server.ts
@@ -0,0 +1,31 @@
+import cloudinary from "cloudinary";
+import { writeAsyncIterableToWritable } from "@remix-run/node";
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET_KEY,
+});
+
+async function uploadImage(data: AsyncIterable<Uint8Array>) {
+  const uploadPromise = new Promise(async (resolve, reject) => {
+    const uploadStream = cloudinary.v2.uploader.upload_stream(
+      {
+        folder: "remix",
+      },
+      (error, result) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve(result);
+      }
+    );
+    await writeAsyncIterableToWritable(data, uploadStream);
+  });
+
+  return uploadPromise;
+}
+
+// console.log("configs", cloudinary.v2.config());
+export { uploadImage };
